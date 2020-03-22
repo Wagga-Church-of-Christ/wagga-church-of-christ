d6cfdd7488e245b038501a17a807726600625482
Simon Biggs <me@simonbiggs.net>
2020-03-22 22:11:30 +1100

    Update HomePageMeetingTimes.tsx

--- src/components/home/HomePageMeetingTimes.tsx
+++ src/components/home/HomePageMeetingTimes.tsx
@@ -7,6 +7,21 @@ const HomePageMeetingTimes = (props) => {
     <div className="row">
       <div className="offset-md-2 col-md-8">
         <div className="home-page-meeting-times">
+          <h2>CURRENT COVID-19 ARRANGEMENTS</h2>
+          
+          <p>
+            We meet as a church online and via a group phone call
+            on Sunday's at 10:00 AM.
+            
+            You can join the meeting by clicking the following link
+            on either a computer, mobile phone, or tablet --
+            <a href=https://meet.google.com/kjs-siny-oow><b>Link to Online Video Conference</b></a>
+            
+            If it is easier for you, you may also join the meeting
+            by ringing +61 2 8320 4510 
+            and when prompted provide the pin 931 759 814 8738#.
+          </p>
+          
           <h2>AS A CHURCH WE MEET TOGETHER ON SUNDAYS AT 10:00 AM</h2>
           <br />
           <h3>
